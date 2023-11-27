1a19ee47940193c2ec8cce4ee2a2112b15cf0452
Clemens Boos <CBoos@dspace.de>
2023-11-27 11:54:43 +0100

    Add demo FMUs for FMI-LS-BUS
    This includes:
    - A demo FMU for a CAN node with triggered output clocks
    - A demo FMU for a simple CAN bus simulation
    
    Each demo includes a script to pack it into a source-code FMU

---
+++ ls-bus-guide/demos/can-node-triggered-output/PackFmu.py
@@ -0,0 +1,41 @@
+import urllib.request
+import zipfile
+
+from pathlib import Path
+
+
+# Output FMU path
+FMU_PATH = Path('DemoCanNodeTriggeredOutput.fmu')
+
+# Repository to fetch the LS-BUS headers from
+LS_BUS_REPO = 'modelica/fmi-ls-bus'
+LS_BUS_REV = '473bd5b80730c47373bf41f1c31d44f50de82dd0'
+LS_BUS_HEADERS = [ 'fmi3LsBus.h', 'fmi3LsBusCan.h' ]
+
+
+def main():
+    demo_dir = Path(__file__).parent
+
+    with zipfile.ZipFile(FMU_PATH, 'w') as fmu:
+        # Add LS-BUS headers from GitHub repository
+        for ls_bus_header in LS_BUS_HEADERS:
+            with urllib.request.urlopen(f'https://raw.githubusercontent.com/{LS_BUS_REPO}/{LS_BUS_REV}/headers/{ls_bus_header}') as f:
+                fmu.writestr(f'sources/{ls_bus_header}', f.read())
+
+        # Add LS-BUS utility headers
+        for file in (demo_dir.parent.parent / 'headers').iterdir():
+            fmu.write(file, f'sources/{file.name}')
+
+        # Add source files
+        for file in (demo_dir / 'src').iterdir():
+            fmu.write(file, f'sources/{file.name}')
+
+        # Add description files
+        fmu.write(demo_dir / 'description' / 'modelDescription.xml', 'modelDescription.xml')
+        fmu.write(demo_dir / 'description' / 'buildDescription.xml', 'sources/buildDescription.xml')
+        fmu.write(demo_dir / 'description' / 'terminalsAndIcons.xml', 'terminalsAndIcons/terminalsAndIcons.xml')
+        fmu.write(demo_dir / 'description' / 'fmi-ls-manifest.xml', 'extra/org.fmi-standard.fmi-ls-bus/fmi-ls-manifest.xml')
+
+
+if __name__ == '__main__':
+    main()
